e08cc952740ac15b0b2aa58fba2694d0a7f6bdf7
Abhishek Raibagi <91134432+abhirai143@users.noreply.github.com>
2023-07-12 23:46:29 +0530

    Create countDownAndUp.py

---
+++ countDownAndUp.py
@@ -0,0 +1,12 @@
+def countDownAndUp(number):
+    print(number)
+    if number == 0:
+        #BASE CASE
+        print('Reached the base case.')
+        return
+    else:
+        #RECURSIVE CASE
+        countDownAndUp(number - 1)
+        print(number)
+        return
+countDownAndUp(3)
