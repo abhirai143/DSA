08063b38814de2297087c74398a41aa52713838c
Abhishek Raibagi <91134432+abhirai143@users.noreply.github.com>
2023-08-09 23:56:24 +0530

    Create TemperatureConvert.py
    TemperatureConvert

---
+++ python/TemperatureConvert.py
@@ -0,0 +1,5 @@
+taking input from the user
+fahrenheit = float(input("Please give the Hahrenheit Temperature : "))  
+#converting Celsius into Fahrenheit 
+celsius = ((fahrenheit-32)*5)/9
+print("Celcius= ",celsius)
