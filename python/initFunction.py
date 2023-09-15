964f4b62072c33019cfa0582e297f414115aa714
Abhishek Raibagi <91134432+abhirai143@users.noreply.github.com>
2023-09-15 23:45:25 +0530

    Create initFunction.py
    add init function

---
+++ python/initFunction.py
@@ -0,0 +1,8 @@
+class Student:
+   def __init__(self, fname, lname, age, section):
+       self.firstname = fname
+       self.lastname = lname
+       self.age = age
+       self.section = section
+# creating a new object
+stu1 = Student("Sara", "Ansh", 22, "A2")
