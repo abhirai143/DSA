4154870d486564ee9543d3be50e70c6f7b762861
Abhishek Raibagi <91134432+abhirai143@users.noreply.github.com>
2023-09-16 17:53:42 +0530

    Create greatestOfThreNo.py
    add greatest of three number

---
+++ python/greatestOfThreNo.py
@@ -0,0 +1,9 @@
+n1 = int(input("please give first number n1: "))
+n2 = int(input("please give second number n2: "))
+n3 = int(input("please give third number n3: "))
+if n1>=n2 and n1>=n3: 
+	print(" n1 is greatest");
+if n2>=n1 and n2>=n3:
+	print(" n2 is greatest");
+if n3>=n1 and n3>=n2:
+	print("n3 is greatest"); 
